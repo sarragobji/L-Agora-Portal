922c1293f398d04220dd8ca84acab61d05e978a6
sarragobji <sarra.gobji2@gmail.com>
2026-05-12 23:06:49 +0100

    Save changes before pulling

--- agora_back/users/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import UtilisateurViewSet
-
-router = DefaultRouter()
-
-router.register(r'utilisateurs', UtilisateurViewSet)
-
-urlpatterns = router.urls
